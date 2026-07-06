2b67d15ab9cf36093db385d9920fb88ee7402e27
Lucas <lucasjwenski@gmail.com>
2026-07-06 07:33:43 -0300

    Refactor Produto class and API interaction
    Refactor Produto class and improve event handling for form submission. Update API request methods and enhance error handling.

--- CADASTRO_PRODUTOS_V2/FORONTEND/sccript.js
+++ CADASTRO_PRODUTOS_V2/FORONTEND/sccript.js
@@ -1,9 +1,10 @@
-class Produto{
+class Produto {
     #preco;
     #quantidade;
 
-    constructor(nome,preco,quantidade){
-        if(!nome || preco<=0 || quantidade<=0){
+
+    constructor(nome, preco, quantidade) {
+        if (!nome || preco <= 0 || quantidade <= 0) {
             throw new Error("Dados inválidos para o produto");
         }
         this.nome = nome;
@@ -11,110 +12,127 @@ class Produto{
         this.#preco = parseFloat(preco);
         this.#quantidade = parseInt(quantidade);
     }
-
-        get preco(){
-            return this.#preco;
-        }
-
-        get quantidade(){
-            return this.#quantidade;
-        }
-
-        valorTotal(){
-            return this.#preco * this.#quantidade;
-        }
-
-        // Método toJSON()
-        toJSON(){
-            return{
-                nome: this.nome,
-                preco: this.#preco,
-                quantidade: this.#quantidade
-            };
-        } 
+    get preco() {
+        return this.#preco;
+    }
+    get quantidade() {
+        return this.#quantidade;
     }
 
-    // const produtos=[];
-    // MUDANÇA DE ARQUITETURA - CLIENTE-SERVIDOR
+    valorTotal() {
+        return this.#preco * this.#quantidade;
+    }
 
-    // criar uma constatnte com endereço de API (rodando no servidor)
-    const API_URL = "http://localhost:3000/produtos";
+    //Método toJSON
+    toJSON() {
+        return {
+            nome: this.nome,
+            preco: this.#preco,
+            quantidade: this.#quantidade
+        };
+    }
+}
 
-    // REQUISIÇÃO POST - enviar info/dados para o servidor
-    // função deve ser async pois o envio - resposta trafegam na mesma rede
-    // usar await até que o servidor responda
+// const produtos = [];
+// MUDANÇA DE ARQUITETURA - CLIENTE-SERVIDOR
 
+// CRIAR UMA CONSTANTE COM ENDEREÇO DE API (RODANDO NO SERVIDOR)
+const API_URL = 'http://localhost:3000/produtos';
 
-    document.getElementById("produto-form").addEventListener("submit",function(e){
-        e.preventDefault();
-
-        const nome = document.getElementById("nome").value;
-        const preco = document.getElementById("preco").value;
-        const quantidade = document.getElementById("quantidade").value;
-
-        try{
-            const novoProduto = new Produto(nome, preco, quantidade);
-
-            // produtos.push(novoProduto);
-
-            // DISPARO DE REDE: envia o produto convertido em textoJSON para o Express
-            const resposta = await fetch(API_URL,{
-                method: 'POST',
-                headers: {'Content-Type':'application/json'},
-                body: JSON.stringify(novoProduto.toJson())
-            });
-
-            if(!resposta.ok){
-                throw new Error("Erro ao salvar o produto no sevidor backend");
-            }
+// REQUISIÇÃO POST - enviar dados para o servidor
+// funcão deve serasync pois o envio, resposta trafegame pela mesma rede
+// usar await até que o servidor responda 
 
 
-            renderizarTabela();
-            e.target.reset();
+document.getElementById("produto-form").addEventListener('submit', async function (e) {
+    e.preventDefault();
 
-        }catch(erro){
-            alert(erro.message);
+    const nome = document.getElementById('nome').value;
+    const preco = document.getElementById('preco').value;
+    const quantidade = document.getElementById('quantidade').value;
+
+    try {
+        const novoProduto = new Produto(nome, preco, quantidade);
+
+        // produtos.push(novoProduto);
+
+        // DISPARO NA REDE: envia o produto convertido em texto JSON para o Express
+
+        const resposta = await fetch(API_URL, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(novoProduto.toJSON())
+        });
+
+        if (!resposta.ok) {
+            throw new Error('Erro ao salvar o produto no servidor backend');
         }
 
+        renderizarTabela();
+        e.target.reset();
 
-    }); //PAREI AQUI
 
-    function renderizarTabela(){
+    } catch (erro) {
+        alert(erro.message);
+    }
+}); //PAREI AQUI
+
+// REQUISAÇÃO GET (buscar um servidor e desenhar tela)
+// O coração da proposta ocorre aqui
+
+async function renderizarTabela() {
+
+    try {
+
+        // buscar dados
+        const resposta = await fetch(API_URL);
+        const dadosBrutosDoServidor = await resposta.json();
+
         const tabela = document.querySelector('#tabela-produtos tbody');
         tabela.innerHTML = '';
+        let totalAcumulado = 0;
 
-        produtos.forEach((produto,index)=>{
+        // 2 passado por cada item retornando pelo BACKEND 
+        dadosBrutosDoServidor.forEach((dados) => {
+            const produto = new Produto(dados.nome, dados.preco, dados.quantidade);
+            totalAcumulado += produto.valorTotal();
+            // desenha a linha na tabela utilizando os dados do objeto reconstruindo 
             const row = document.createElement('tr');
 
-            row.innerHTML=`
+            row.innerHTML = `
                 <td>${produto.nome}</td>
                 <td>R$ ${produto.preco.toFixed(2)}</td>
                 <td>${produto.quantidade}</td>
-                <td></td>
+                <td>R$ ${produto.valorTotal().toFixed(2)}</td>
             `;
-
-            const botaoRemover = document.createElement('button');
-            botaoRemover.textContent = 'Remover';
-            botaoRemover.addEventListener('click',()=>removerProduto(index));
-
-            row.querySelector('td:last-child').appendChild(botaoRemover);
             tabela.appendChild(row);
         });
 
-        atualizarTotal();
+        // 3 atualiza o elemento de texto com o acumulado total
+        document.getElementById('total-estoque').textContent = `Total em estoque: R$ ${totalAcumulado.toFixed(2)}`;
+    } catch (erro) {
+        console.error("Erro ao buscar dados no servidor:", erro);
     }
+};
 
-    function atualizarTotal(){
-        const total = produtos.reduce((acc,p)=>acc+p.valorTotal(),0);
-        document.getElementById('total-estoque').textContent = `Total em estoque: R$ ${total.toFixed(2)}`;
+// REQUISIÇÃO PARA DELETE (apagar os dados em lote)
+document.getElementById('limpar-tabela').addEventListener('click', async function () {
+    if (confirm("Deseja mesmo limpar mesmo toda a tabela no servidor?")){
+        try {
+            // envia uma ordem de remoção para a API
+            await fetch(API_URL, { method: 'DELETE' });
+
+            // atualiza a tabela
+            renderizarTabela();
+        } catch (erro) {
+            console.error("Erro ao limpar dados no servidor:", erro);
+        }
     }
+});
 
-    function removerProduto(index){
-        produtos.splice(index,1);
-        renderizarTabela();
-    }
+// INICIALIAZAÇÃO AUTOMÁTICA 
+// assim que o usuário abre o navegador, o app busca
+// se ja há dados salvods de sessões anteriores lá no BACKEND
+renderizarTabela();
 
-    document.getElementById('limpar-tabela').addEventListener('click',function(){
-        produtos.length = 0;
-        renderizarTabela();
-    });
+
